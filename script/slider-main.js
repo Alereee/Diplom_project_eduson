5eaef5015331a3fa448c6a7a1edc3bb09060f804
Alereee <ilyamelehin1999@gmail.com>
2026-08-06 23:05:18 +0300

    Initial cottt

--- script/slider-main.js
+++ script/slider-main.js
@@ -3,18 +3,23 @@ const moveSlider = (direction) => {
     parseFloat(
       window.getComputedStyle(document.querySelector(".movie-card")).width,
     ) + parseFloat(window.getComputedStyle(sliderTrack).gap);
-  const targetScroll = sliderTrack.scrollLeft + direction * scrollAmount;
+  let targetScroll = sliderTrack.scrollLeft + direction * scrollAmount;
   console.log(targetScroll);
   sliderTrack.scrollTo({
     left: targetScroll,
     behavior: "smooth",
   });
-  if (targetScroll === 0)
+  console.log(targetScroll);
+  if (targetScroll <= 10){
     document.querySelector(".arrow-left").classList.add("disabled");
+    targetScroll = 0;
+    sliderTrack.scrollLeft = 0;
+  }
   if (targetScroll > 0)
     document.querySelector(".arrow-left").classList.remove("disabled");
-  if (targetScroll < sliderTrack.scrollWidth - sliderTrack.clientWidth)
+  if (targetScroll - 10 < sliderTrack.scrollWidth - sliderTrack.clientWidth)
     document.querySelector(".arrow-right").classList.remove("disabled");
+    
   if (targetScroll >= sliderTrack.scrollWidth - sliderTrack.clientWidth)
     document.querySelector(".arrow-right").classList.add("disabled");
 };
