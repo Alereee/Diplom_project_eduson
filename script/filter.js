f2f944fcca94b04bf4c0965f95426f25945f013f
Alereee <ilyamelehin1999@gmail.com>
2026-08-10 23:18:13 +0300

    filter-select

--- script/filter.js
+++ script/filter.js
@@ -1,12 +1,35 @@
 import { moviesConfig } from "./config-movies-films.js";
 
-const filterGanre = document.querySelector(".genre");
-const filterRating = document.querySelector(".rating");
-const filterCountry = document.querySelector(".country");
-const filterYear = document.querySelector(".year");
-
-filterGanre.forEach((genre) => {
-  genre.addEventListener("click", (e) => {
-    console.log(e.target);
+const configFilter = [
+   { 
+    genre: document.querySelector("[name='genre']"),
+    optionsDefault: "Все жанры",
+   },
+   { 
+    rating:  document.querySelector("[name='rating']"),
+    optionsDefault: "Выбрать рейтинг",
+   },
+   { 
+    country: document.querySelector("[name='country']"),
+    optionsDefault: "Все страны",
+   },
+   { 
+    year: document.querySelector("[name='year']"),
+    optionsDefault: "Все годы",
+   },
+]
+const optionSelect = (select, array, defaultText) => {
+  select.innerHTML = "";
+  select.appendChild(document.createElement("option")).textContent = defaultText;
+  array.forEach((item) => {
+    select.appendChild(document.createElement("option")).textContent = item;
   });
-});
+};
+for(let item of configFilter) {
+  optionSelect(
+    item[Object.keys(item)[0]],
+    [...new Set(moviesConfig.map((movie) => movie[Object.keys(item)[0]]))]
+    .sort((a, b) => typeof a === "number" ? b - a : a.localeCompare(b)),
+    item.optionsDefault,
+  );
+};
